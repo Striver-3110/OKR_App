ded1776a8acb2dec5849870a4943a704cd98676c
striver-3110 <21csjay067@ldce.ac.in>
2025-02-17 23:05:45 +0530

    feat: apply Incubyte color palette and refactor OKRInputForm

--- client/src/components/OKRForm/ObjectiveInput.tsx
+++ client/src/components/OKRForm/ObjectiveInput.tsx
@@ -10,29 +10,25 @@ export const ObjectiveInput = ({newObjective, setNewObjective, handleGenerateKey
   return (
     <>
       <div
-        className="w-full flex flex-col gap-4 px-10 py-4 w-full bg-gray-50 shadow-md shadow-gray-400 rounded-md border border-gray-400">
+        className="w-full flex flex-col gap-4 px-10 py-4 w-full bg-gray-300  rounded-md border border-gray-400 z-0">
 
         <div className="w-full relative flex flex-row gap-6">
           <label htmlFor="objective"
-                 className="absolute -top-[10px] ml-4 bg-gray-200 px-1 py-[2px] rounded-md text-gray-900 text-xs">Objective</label>
+                 className=" absolute -top-[10px] ml-4 px-1 py-[2px] rounded-md bg-[#5698A7] text-gray-50 text-sm">Objective</label>
           <input
             id="objective"
-            className="w-full py-3 px-4 rounded-lg text-gray-900 focus:outline-none bg-gray-300 shadow-sm border border-gray-600 rounded-lg focus:ring-1 focus:ring-gray-600 text-gray-900 "
+            className="w-full py-3 px-4 rounded-lg text-gray-900 focus:outline-none bg-gray-100  border border-gray-600 rounded-lg focus:ring-1 focus:ring-gray-600 text-gray-900 "
             type="text"
             value={newObjective}
             onChange={(e) => setNewObjective(e.target.value)}
             placeholder="Enter objective"
           />
-          <div className=" flex justify-end relative">
-  <span className="absolute flex size-5 -top-[5px] -right-[8px]">
-  <span
-    className="absolute inline-flex h-full w-full animate-ping rounded-full bg-green-500 opacity-75"></span>
-    <span className="relative inline-flex size-5 rounded-full bg-green-500"></span>
-    </span>
+          <div className=" flex justify-end relative p-2 bg-transparent z-10">
             <button
               onClick={() => handleGenerateKeyResults()}
-              className="animate- bg-gradient-to-b from-sky-500 to-indigo-500 px-4 py-2 rounded-md text-white shadow-md ">
-              Generate
+              id="ai-animate-btn"
+              className="bg-gradient-to-r from-[#143D60] to-[#27667B] px-4 py-2 rounded-full relative text-white ">
+              Generate Key Results
             </button>
           </div>
         </div>
